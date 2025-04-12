7865b6faae6a37bdfecbafbbc895c88c1f13ebf5
Iroshan <hello@iroshandezilva.com>
2025-04-12 10:53:09 +0530

    Remove anchor tag and replace with Link Component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <header className="w-full border-b">
       <div className="max-w-5xl mx-auto px-4 py-3 flex justify-between items-center">
         <span className="font-bold">Iroshan</span>
         <nav className="space-x-4">
-          {/* <a href="/">Home</a> */}
-          <a href="/about">About</a>
-          <a href="/blog">Writings</a>
-          <a href="/stack">Stack</a>
-          <a href="/contact">Contact</a>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/blog">Writings</Link>
+          <Link href="/stack">Stack</Link>
+          <Link href="/contact">Contact</Link>
         </nav>
       </div>
     </header>
