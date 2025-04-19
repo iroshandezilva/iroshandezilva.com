192f1e909730b24c6204b2273ea08ef4e5ace289
Iroshan <hello@iroshandezilva.com>
2025-04-19 15:54:55 +0530

    add mdx styles

--- src/components/MdxRenderer.tsx
+++ src/components/MdxRenderer.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { mdxComponents } from "@/lib/mdx-components";
 
 interface MdxRendererProps {
-	source: MDXRemoteSerializeResult;
+  source: MDXRemoteSerializeResult;
 }
 
 export default function MdxRenderer({ source }: MdxRendererProps) {
-	return <MDXRemote {...source} components={{}} />;
+  return <MDXRemote {...source} components={mdxComponents} />;
 }
