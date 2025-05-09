5187bd2ca23ec18b8cf879851e7fdac67498b529
Iroshan <hello@iroshandezilva.com>
2025-05-09 16:04:09 +0530

    feat: add AuroraText component for animated gradient text and update Hero section

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,25 +3,19 @@
 import { CustomLink } from "@/components/shared/3d-button";
 import { ArrowRight } from "@phosphor-icons/react";
 import Image from "next/image";
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 export default function Hero() {
   return (
     <section className="mx-auto max-w-3xl px-6 pt-16 pb-12 md:pt-28 lg:px-0">
       <div className="flex flex-col gap-6 lg:gap-12">
         <div className="flex flex-col justify-items-center gap-5 md:px-16 lg:px-36">
-          {/* <div className="block w-24 overflow-hidden rounded-full md:hidden md:w-32">
-            <Image
-              src="/images/iroshandezilva_ux_portrait.webp"
-              alt="Hero image"
-              width={0}
-              height={0}
-              sizes="100vw"
-              className="h-auto w-full rounded-lg"
-              priority
-            />
-          </div> */}
           <h1 className="font-heading text-center text-4xl leading-12 text-gray-900 lg:text-5xl lg:leading-16">
-            Users Are Leaving Your SaaS I’ll Help You Keep Them
+            Users Are Leaving Your SaaS{" "}
+            <AuroraText colors={["#FF0080", "#7928CA", "#0070F3", "#38bdf8"]}>
+              {" "}
+              I’ll Help You Keep Them
+            </AuroraText>
           </h1>
           <p className="font-body justify-start text-center text-base leading-6 font-normal text-slate-700 md:px-6 lg:px-12">
             By refining your product’s UX, I help you increase activation rates
